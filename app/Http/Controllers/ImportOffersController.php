054a0965da1bd2ba0d3a2e00fb8ee3ea6376cc9f
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-10-12 17:24:25 -0300

    feat: add import offers feature structure

--- app/Http/Controllers/ImportOffersController.php
+++ app/Http/Controllers/ImportOffersController.php
@@ -6,7 +6,8 @@ use App\UseCases\Interfaces\OfferUseCaseInterface;
 
 class ImportOffersController extends Controller
 {
-    public function __construct(private readonly OfferUseCaseInterface $offerUseCase
+    public function __construct(
+        private readonly OfferUseCaseInterface $offerUseCase
     ) {}
 
     public function __invoke(): void
